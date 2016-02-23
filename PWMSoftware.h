94554113178f573578d541ee69c51001ad9011e1
starflame765 <starflame765@localhost>
2016-02-23 15:13:52 +0800

    Fixing the repo

---
+++ PWMSoftware.h
@@ -0,0 +1,21 @@
+#ifndef PWMSoftware_h
+#define PWMSoftware_h
+
+#include "Arduino.h"
+
+class PWMSoftware
+{
+	public:
+	PWMSoftware(uint8_t timerNo, uint8_t ocrLetter);
+	void setPeriod1(uint16_t noofTicks);
+	void setPWM(uint16_t noofTicks);
+	private:
+	volatile uint16_t *_OCRnX;
+	volatile uint8_t *_TCCRnA;
+	volatile uint16_t *_ICRn;
+	volatile uint8_t *_TCCRnB;
+	uint8_t _timerNo;
+	uint8_t _ocrLetter;
+	
+};
+#endif
