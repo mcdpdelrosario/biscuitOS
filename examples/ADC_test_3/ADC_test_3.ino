db035cf0f39a4e2072595991eab0317db0a414be
starflame765 <starflame765@localhost>
2016-02-24 19:16:39 +0800

    ADC module Functions: enable getReading

--- examples/ADC_test_3/ADC_test_3.ino
+++ examples/ADC_test_3/ADC_test_3.ino
@@ -1,12 +1,12 @@
-#include <ADCv1.h>
-ADCv1 adc1;
+#include <ADCv2.h>
+ADCv2 adc1;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  for(uint8_t a=0;a<5;a++){
+  /*for(uint8_t a=0;a<5;a++){
   adc1.enable(a);
-  }
-  adc1.setMode(1);
+  }*/
+  adc1.enable(0);
   
   
 }
@@ -14,9 +14,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   delay(1000);
-  for(uint8_t b=0;b<6;b++){
+  /*for(uint8_t b=0;b<6;b++){
   Serial.print(adc1.getReading(b));
   Serial.print("\t");
   }
-  Serial.print("\n");
+  Serial.print("\n");*/
+  Serial.println(adc1.getReading(0));
 }
