c0f629844b0a91108cae0c8749f1c31271ec0117
angel <angel@localhost>
2016-03-08 20:47:42 +0800

    Worked with the proportional controller

---
+++ Motor.h
@@ -0,0 +1,31 @@
+
+
+#ifndef Motor_h
+#define Motor_h
+
+
+#include "Arduino.h"
+#include "PWMSoftware.h"
+
+class Motor
+{
+  public:
+    PWMSoftware motorControl;
+    void initialize(byte num, uint8_t motorPWMPin, uint8_t motorDirectionPin, float diameter);
+    void setPeriod(byte num, uint16_t period);
+    void setTime(byte num, int percent);
+    uint16_t getSpeed(byte num);  
+    void setTarget(byte num, uint16_t targetSpeed);
+    void correctSpeed(byte num);
+    void computeSpeed(byte num);
+  private:
+  	uint8_t _motorPWMPin;
+ 	  uint8_t _motorDirectionPin;
+  	uint16_t _targetspeedTime;
+  
+  
+
+};
+static Motor motors;
+#endif
+
---
+++ examples/biscuit_allin_test_1/biscuit_allin_test_1.ino
@@ -0,0 +1,30 @@
+#include <ADCv2.h>
+#include <biscuitos.h>
+#include <Motor.h>
+#include <PWMSoftware.h>
+#include <ServoEnhance.h>
+#include <UARTv1.h>
+#include <Wifi.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  rScheduler.initialize();
+  rADC.enable(0);
+  rScheduler.addProcess(technicaldept,16);
+  rScheduler.addProcess(printADC0Reading,1000);
+  rScheduler.start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void printADC0Reading(){
+  Serial.println(rADC.getReading(0));
+}
+void technicaldept(){
+  
+}
+
