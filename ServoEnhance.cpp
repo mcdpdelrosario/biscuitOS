26957f9bf512c8c5237f4f561eb372a7b9d98d43
starflame765 <starflame765@localhost>
2016-03-08 11:46:43 +0800

    Added Servo enhanced

---
+++ ServoEnhance.cpp
@@ -0,0 +1,67 @@
+#include "Arduino.h"
+#include "ServoEnhance.h"
+#include "PWMSoftware.h"
+
+ 
+
+ServoEnhance::ServoEnhance(){
+
+}
+
+void ServoEnhance::attach(uint8_t pin){
+	
+	
+_pin = pin;
+
+switch(_pin){
+//Timer 3
+case 3:
+	p.init(3,3);
+	p.setPeriod(312);
+	 Serial.println("JOSHUA POGI 3");
+		break;
+case 2:p.init(3,2);
+		p.setPeriod(312);
+	 Serial.println("JOSHUA POGI 2");
+		break;
+case 5:p.init(3,1);
+	p.setPeriod(312);
+		 Serial.println("JOSHUA POGI 5");
+		break;
+		//Timer 1
+case 6:p.init(1,3);
+	p.setPeriod(312);
+		 Serial.println("JOSHUA POGI 6");
+		break;
+case 7:p.init(1,2);
+	p.setPeriod(312);
+ Serial.println("JOSHUA POGI 7");
+		break;
+case 9:p.init(1,1);
+	p.setPeriod(312);
+		 Serial.println("JOSHUA POGI 9");
+		break;
+default: Serial.println("JOSHUA POGI 123");
+	
+	
+}
+
+
+}
+
+void ServoEnhance::setAngle(uint16_t angle){
+	uint16_t y;
+	y = ((angle*29)/180) + 9;
+	p.setPWM(y);
+}
+
+
+
+void ServoEnhance::detach(uint8_t dpin){
+	
+	
+	
+	
+	
+}
+
