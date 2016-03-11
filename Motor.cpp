4a75ee2d7547790324066570d01b1c245418f334
angel <angel@localhost>
2016-03-11 19:31:14 +0800

    Tried the working code using the battery         Update:         1. Max number of ticks for 250mS correctSpeed is 9         2. To have proper direction, place the tape wire on pin 44

--- Motor.cpp
+++ Motor.cpp
@@ -26,20 +26,20 @@ struct motors m[MAX_MOTORS];
 byte _num;
 void Motor::initialize(byte num, uint8_t motorPWMPin, uint8_t motorDirectionPin, float diameter){
 
-  EICRB = B10100000;  //enable int 7 and int 6 falling edge
-  EIMSK = B11000000;
-  m[num].motorPWMPin = motorPWMPin;
+  EICRB = B10100000;                                              //ena ble int 7 and int 6 falling edge
+  EIMSK = B11000000;  
+  m[num].motorPWMPin = motorPWMPin;                               //passed the value from the user to the variable struct motorPWMPin
 
 
   if(motorDirectionPin==42) 
   {
-    DDRG = B00010000;
+    DDRG = B00010000;                                              //assembly code setting the pin 42(Direction Pin) as output
     m[num].motorDirectionPin = DDRG;
   }
 
  if(m[num].motorPWMPin==6)
   {
-    m[num].motorControl.init(1,2);
+    m[num].motorControl.init(1,2);                                //Initializing the timers that will be used based on the PWMPin set by the user
   }
 
   else if(m[num].motorPWMPin==7)
@@ -60,11 +60,14 @@ void Motor::initialize(byte num, uint8_t motorPWMPin, uint8_t motorDirectionPin,
 
 void Motor::changeDirection(byte num, byte dir)
 {
+
+  m[0].flag=0;                                                  //reset the flag used by the interrupt for changeDirection
+  m[1].flag=0;
   if(num==0)
   {
     if(dir==1)
     {
-       PORTG = B00010000; 
+       PORTG = B00010000;                                     //Assembly code for digitalWrite of a pin, setting 1 as high and 0 as low
     }
 
     else if(dir==0)
@@ -72,44 +75,47 @@ void Motor::changeDirection(byte num, byte dir)
       PORTG = 0x00;
     }
   }
+
+  
+
 }
 
-void Motor::setPeriod(byte num, uint16_t period)
+void Motor::setPeriod(byte num, uint16_t period)            //User sets the total period of the motor 
 {
  uint16_t numberofticksTime;
  
   m[num].period =period;
   if (m[num].period > 20)
-  {
+  {                                                        //Period to be converted to the number of ticks for the PWMSoftware function setPeriod
     m[num].period = 20;
   }
   numberofticksTime = m[num].period / (64e-3);
   m[num].motorControl.setPeriod(numberofticksTime);
 
 }
-
-void Motor::setTime(byte num, int percent)
+    
+void Motor::setTime(byte num, int percent)                //Acts as the setDuty cycle, input is in percent
 {
   uint16_t numberofticksTimeD;
   uint16_t percentDuty;
-  m[num].percent = percent;                          
+  m[num].percent = percent;                               //Percent to be converted again to the number of ticks
   percentDuty = (m[num].period*m[num].percent)/100;                   
   numberofticksTimeD = percentDuty/(64e-3); 
   m[num].motorControl.setPWM(numberofticksTimeD);
 
 }
 
-void Motor::setTarget(byte num, uint16_t targetSpeed)
-{
+void Motor::setTarget(byte num, uint16_t targetSpeed)     //function used by the user to set the target ticks
+{ 
  m[num].targetSpeed = targetSpeed;
 }
 
 
 void Motor::correctSpeed(byte num)
 {
-
+                                                        //Application of proportional controller to correct the speed and reach the target ticks of the user
  
-  computeSpeed(num);
+  computeSpeed(num);                                    //Printing the actual speed function
   uint8_t proportionalConstant = 5;
 
   if(m[num].actualSpeed>m[num].targetSpeed)
@@ -136,7 +142,7 @@ void Motor::correctSpeed(byte num)
 
 }
 
-void Motor::computeSpeed(byte num)
+void Motor::computeSpeed(byte num)                    //Getting the actual ticks and passing it to the variable struct actualSpeed.
 {
   uint32_t temp;
   uint32_t timePassed;
@@ -147,13 +153,13 @@ void Motor::computeSpeed(byte num)
   m[num].actualSpeed = temp;
 }
 
-uint16_t Motor::getSpeed(byte num)
+uint16_t Motor::getSpeed(byte num)                    //Function that returns the value of the actual Speed
 {
   return m[num].actualSpeed;
 }
 
 
-void Motor::getDirection()
+void Motor::getDirection()                            // FUnction that determines the direction of the motor
 {
   if(m[1].flag==1)
   {
@@ -170,7 +176,8 @@ void Motor::getDirection()
 }
 
 
-ISR(INT6_vect)
+ISR(INT6_vect)                                       //Interrupts used. Once it hits int6 before int 7, it means forward. Then the number of rotations will just
+                                                     //increment every falling edge.
 {
   if(m[1].flag==1)
   {
@@ -184,9 +191,9 @@ ISR(INT6_vect)
   m[0].rotations++;
 }
 
-ISR(INT7_vect)
+ISR(INT7_vect)                                      //Same with interrupt 6 but if it hits first before interrupt 6, means the motor is in backward direction. 
 {
-  if(m[0].flag==1)
+  if(m[0].flag==1)            
   {
     m[1].flag=0;
   }
