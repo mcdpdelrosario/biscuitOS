2707bc667a78ea467508cd4fcc7ba3c4db0ab7d8
angel <angel@localhost>
2016-03-11 19:31:14 +0800

    Tried the working code using the battery         Update:         1. Max number of ticks for 250mS correctSpeed is 9         2. To have proper direction, place the tape wire on pin 44

--- Motor.h
+++ Motor.h
@@ -11,15 +11,15 @@ class Motor
 {
   public:
     PWMSoftware motorControl;
-    void initialize(byte num, uint8_t motorPWMPin, uint8_t motorDirectionPin, float diameter);
-    void setPeriod(byte num, uint16_t period);
-    void setTime(byte num, int percent);
-    uint16_t getSpeed(byte num);  
-    void setTarget(byte num, uint16_t targetSpeed);
-    void correctSpeed(byte num);
-    void computeSpeed(byte num); 
-    void getDirection();
-    void changeDirection(byte num, byte dir);
+    void initialize(byte num, uint8_t motorPWMPin, uint8_t motorDirectionPin, float diameter);        //initialize the values to be used for PWMPin, DirectionPin, Diameter of the motor
+    void setPeriod(byte num, uint16_t period);                                                        //Set the total period for the motor example is 20 which corresponds to 20ms
+    void setTime(byte num, int percent);                                                              //Set the PWM of the motor, in percent
+    uint16_t getSpeed(byte num);                                                                      //Function that returns the speed of the motor        
+    void setTarget(byte num, uint16_t targetSpeed);                                                   //Used by the user to set the target ticks the user wants the motor to reach
+    void correctSpeed(byte num);                                                                      //Function that always runs to reach the target ticks set by the user
+    void computeSpeed(byte num);                                                                      //Function that passes the number of rotations from the interrupt to a variable and computes the speed based on the rotations
+    void getDirection();                                                                              //Function that prints the direction of the motor
+    void changeDirection(byte num, byte dir);                                                         //Function that sets the direction of the motor based on the users input. 1 means forward, 0 means backward
   private:
   	uint8_t _motorPWMPin;
  	  uint8_t _motorDirectionPin;
