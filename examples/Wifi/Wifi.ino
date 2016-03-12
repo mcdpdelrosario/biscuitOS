91effe0da2e7fe635c0f852f68972efbed2947d6
DP <DP@localhost>
2016-03-12 20:51:25 +0800

    Update: 1. Able to add command in .ino.

--- examples/Wifi/Wifi.ino
+++ examples/Wifi/Wifi.ino
@@ -6,12 +6,12 @@
 #include <UARTv1.h>
 #include <Wifi.h>
 
-
-
 void setup() {
+  Transceiver.start(9600);
   // put your setup code here, to run once:
-  Wifi.initialize(9600,"yshie","88888888"); //Wifi.initialize(baud,apName,password);
+  Wifi.initialize(9600,"PLDTHOMEDSL_YSHIE","88888888"); //Wifi.initialize(baud,apName,password);
   Wifi.listen("8000","192.168.1.7"); //Wifi.listen(portNumber,ip address);
+  Wifi.commands(control);
   rScheduler.initialize();
   rScheduler.addProcess(technicaldept,16);
 //  rScheduler.addProcess(quickFix,8);
@@ -22,8 +22,54 @@ void loop() {
   // put your main code here, to run repeatedly:
   Wifi.receive();
 }
-void quickFix(){
-  
+char myCommand;
+
+void control(char Command) {
+
+  switch (Command) {
+    case 'X':
+      digitalWrite(42,LOW);
+      digitalWrite(43,LOW);
+      //motorLeft.setPWM(10);
+      //motorRight.setPWM(10);
+        Transceiver.print("S\n");
+      break;
+    case 'W':
+      digitalWrite(42,LOW);
+      digitalWrite(43,LOW);
+      //motorLeft.setPWM(300);
+      //motorRight.setPWM(300);
+        Transceiver.print("W\n");
+      break;
+    case 'D':
+      digitalWrite(42,LOW);
+      digitalWrite(43,LOW);
+      //motorLeft.setPWM(150);
+      //motorRight.setPWM(300);
+        Transceiver.print("D\n");
+      break;
+    case 'A':
+      digitalWrite(42,LOW);
+      digitalWrite(43,LOW);
+      //motorLeft.setPWM(300);
+      //motorRight.setPWM(150);
+        Transceiver.print("A\n");
+      break;
+
+    case 'S':
+      //digitalWrite(42,)
+      digitalWrite(42,HIGH);
+      digitalWrite(43,HIGH);
+      //motorLeft.setPWM(300);
+      //motorRight.setPWM(300);
+        Transceiver.print("S\n");
+      break;
+    case 'F':
+        Transceiver.print("F\n");
+      break;
+    default:
+      break;
+  }
 }
 void technicaldept(){
   
