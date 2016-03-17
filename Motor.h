5567b3b9991900a42e8f9079e2f1912973cca880
starflame765 <starflame765@localhost>
2016-03-17 15:21:17 +0800

    Scheduler fixed for time more than 4000 PWM software more resolution but with maximum 32mS Servo Enhanced more accurate and have more angles

---
+++ Motor.h
@@ -0,0 +1,35 @@
+
+
+#ifndef Motor_h
+#define Motor_h
+
+
+#include "Arduino.h"
+#include "PWMSoftware.h"
+
+class Motor
+{
+  public:
+    PWMSoftware motorControl;
+    void initialize(byte num, uint8_t motorPWMPin, uint8_t motorDirectionPin, float diameter);        //initialize the values to be used for PWMPin, DirectionPin, Diameter of the motor
+    void setPeriod(byte num, uint16_t period);                                                        //Set the total period for the motor example is 20 which corresponds to 20ms
+    void setTime(byte num, int percent);                                                              //Set the PWM of the motor, in percent
+    uint16_t getSpeed(byte num);                                                                      //Function that returns the speed of the motor        
+    void setTarget(byte num, uint16_t targetSpeed);                                                   //Used by the user to set the target ticks the user wants the motor to reach
+    void correctSpeed(byte num);                                                                      //Function that always runs to reach the target ticks set by the user
+    void computeSpeed(byte num);                                                                      //Function that passes the number of rotations from the interrupt to a variable and computes the speed based on the rotations
+    void getDirection();                                                                              //Function that prints the direction of the motor
+    void changeDirection(byte num, byte dir);      
+    void resetFlags();
+                                                       //Function that sets the direction of the motor based on the users input. 1 means forward, 0 means backward
+  private:
+  	uint8_t _motorPWMPin;
+ 	  uint8_t _motorDirectionPin;
+  	uint16_t _targetspeedTime;
+  
+  
+
+};
+static Motor motors;
+#endif
+
