914c4a6680f330c76a67fdb5dd3bbde74ab92336
DP <DP@localhost>
2016-03-21 15:14:47 +0800

    Update: 1. The user will be able to configure TRUE baudrate

---
+++ ServoEnhance.cpp
@@ -0,0 +1,68 @@
+#include "Arduino.h"
+#include "ServoEnhance.h"
+#include "PWMSoftware.h"
+
+ 
+
+ServoEnhance::ServoEnhance(){
+
+}
+
+void ServoEnhance::attach(uint8_t pin){
+	
+	
+_pin = pin;
+
+switch(_pin){
+//Timer 3
+case 3:
+	p.init(3,3);
+	p.setPeriod(40000);
+	 //Serial.println("JOSHUA POGI 3");
+		break;
+case 2:p.init(3,2);
+		p.setPeriod(40000);
+	//Serial.println("JOSHUA POGI 2");
+		break;
+case 5:p.init(3,1);
+	p.setPeriod(40000);
+		// Serial.println("JOSHUA POGI 5");
+		break;
+		//Timer 1
+case 6:p.init(1,3);
+	p.setPeriod(40000);
+		 // Serial.println("JOSHUA POGI 6");
+		break;
+case 7:p.init(1,2);
+	p.setPeriod(40000);
+ // Serial.println("JOSHUA POGI 7");
+		break;
+case 9:p.init(1,1);
+	p.setPeriod(40000);
+		 // Serial.println("JOSHUA POGI 9");
+		break;
+default:
+ // Serial.println("JOSHUA POGI 123");
+	
+	break;
+}
+
+
+}
+
+void ServoEnhance::setAngle(uint16_t angle){
+	uint16_t y;
+	y = ((angle*1800)/180) + 3000;
+	p.setPWM(y);
+}
+
+
+
+void ServoEnhance::detach(uint8_t dpin){
+	
+	
+	
+	
+	
+}
+
