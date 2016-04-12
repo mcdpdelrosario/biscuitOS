9e7bb23d6889229c66e07b359cb54c3377662f66
Angel <Angel@localhost>
2016-04-12 12:07:00 -0700

    Working on new new module correctSpeed

--- Motor.h
+++ Motor.h
@@ -14,11 +14,10 @@ class Motor
     void initialize(byte num, uint8_t motorPWMPin, uint8_t motorDirectionPin, float diameter);        //initialize the values to be used for PWMPin, DirectionPin, Diameter of the motor
     void setPeriod(byte num, uint16_t period);                                                        //Set the total period for the motor example is 20 which corresponds to 20ms
     void setTime(byte num, int percent);                                                              //Set the PWM of the motor, in percent                                                                  //Function that returns the speed of the motor        
-    void setTarget(byte num, int16_t targetTime);                                                        //Function that always runs to reach the target ticks set by the user                                                             //Function that passes the number of rotations from the interrupt to a variable and computes the speed based on the rotations
+    void setTarget(byte num, uint16_t targetTime);                                                        //Function that always runs to reach the target ticks set by the user                                                             //Function that passes the number of rotations from the interrupt to a variable and computes the speed based on the rotations
     void getDirection(byte num);                                                                              //Function that prints the direction of the motor
     void changeDirection(byte num, byte dir);      
-    uint8_t getConda(); 
-    uint8_t getCondb();
+   
 
     // -----
     int16_t presentTime(byte num);  
@@ -28,14 +27,11 @@ class Motor
     int16_t pastError(byte num);
     int16_t currentError(byte num);
     int16_t sumError(byte num);
-    int16_t printPID(byte num);
-    void correctSpeed(byte num);
-    void checkPresentTime(byte num);
-
+    int16_t applyDirection(byte num);
                                                        //Function that sets the direction of the motor based on the users input. 1 means forward, 0 means backward
   private:
   	uint8_t _motorPWMPin;
- 	  uint8_t _motorDirectionPin;
+ 	uint8_t _motorDirectionPin;
   	uint16_t _targetspeedTime;
   
   
